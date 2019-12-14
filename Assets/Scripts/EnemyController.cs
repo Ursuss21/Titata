268519edbcf8d0381231ab45a6a51c1a5cfe83d8
Ursuss21 <robert.skrzypczak21@gmail.com>
2019-12-14 21:39:08 +0100

    Added basic enemy
    Basic enemy, who can wander, follow player and die.
    
    BUGS FOUND
    0001 - colliders don't exist in removed doors (you can walk through the walls)
    0002 - enemy is spawning inside the player and follows him all the time
    
    TO DO
    - remove wandering
    - better method to store enemies inside the rooms
    - generating enemies inside the rooms

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnemyState{
+    Wander,
+    Follow,
+    Die
+};
+
+public class EnemyController : MonoBehaviour
+{
+    GameObject player;
+    public EnemyState currentState = EnemyState.Wander;
+    public float range;
+    public float speed;
+    private bool chooseDirection = false;
+    private bool dead = false;
+    private Vector3 randomDirection;
+    
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        switch(currentState){
+            case EnemyState.Wander:
+                Wander();
+                break;
+            case EnemyState.Follow:
+                Follow();
+                break;
+            case EnemyState.Die:
+                break;
+        }
+
+        if(IsPlayerInRange(range) && currentState != EnemyState.Die){
+            currentState = EnemyState.Follow;
+        }
+        else if(!IsPlayerInRange(range) && currentState != EnemyState.Die){
+            currentState = EnemyState.Wander;
+        }
+    }
+
+    private bool IsPlayerInRange(float range){
+        return Vector3.Distance(transform.position, player.transform.position) <= range;
+    }
+
+    private IEnumerator ChooseDirection(){
+        chooseDirection = true;
+        yield return new WaitForSeconds(Random.Range(2f, 8f));
+        randomDirection = new Vector3(0, 0, Random.Range(0, 360));
+        Quaternion nextRotation = Quaternion.Euler(randomDirection);
+        transform.rotation = Quaternion.Lerp(transform.rotation, nextRotation, Random.Range(0.5f, 2.5f));
+        chooseDirection = false;
+    }
+
+    void Wander(){
+        if(!chooseDirection){
+            StartCoroutine(ChooseDirection());
+        }
+        transform.position = -transform.right * speed * Time.deltaTime;
+        if(IsPlayerInRange(range)){
+            currentState = EnemyState.Follow;
+        }
+    }
+
+    void Follow(){
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+    }
+
+    public void Death(){
+        Destroy(gameObject);
+    }
+}
